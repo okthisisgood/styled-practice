f71502e8a4a150b463e35d5225ea0a37ec321f85
jisoo <jsoo744@gmail.com>
2020-02-26 18:00:54 +0900

    [*] Applied keyframe animation with props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,7 +13,7 @@ function App() {
     <Container>
       <GlobalStyle/>
       <Button>Hello</Button>
-      <Button danger>Bye</Button>
+      <Button danger rotationTime={5}>Bye</Button>
       <Anchor href="https://google.com">Go to google</Anchor>
     </Container>
     
@@ -39,10 +39,24 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => props.danger ? "#fc5c65" : "#26de81" };
+  ${props => {
+    if (props.danger) {
+      return css`animation: ${rotation} ${props.rotationTime}s linear infinite;`
+    }
+  }}
 `
 
 const Anchor = styled(Button.withComponent("a"))`
   text-decoration:none;
-`;
+`
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
 
 export default App;
