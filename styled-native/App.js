8b1923a99e708a5fed3c732a8483b0ad0602313e
jisoo <jsoo744@gmail.com>
2020-02-27 14:29:46 +0900

    [*] Use StyledComponents in Native project

--- styled-native/App.js
+++ styled-native/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 100px
+`
+
+const Title = styled.Text`
+  font-weight: 600;
+  font-size: 32px;
+  text-align: center;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <Title>React Native with Styled Components</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
