10c017564992595c38bf368efe260d47c1ab8d39
jisoo <jsoo744@gmail.com>
2020-02-27 13:28:25 +0900

    [*] Commented Buttons

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function App() {
-  return (
-    <Container>
-      <GlobalStyle/>
-      <Button>Hello</Button>
-      <Button danger rotationTime={5}>Bye</Button>
-      <Anchor href="https://google.com">Go to google</Anchor>
-    </Container>
+  return (<Container></Container>);
+  // return (
+  //   <Container>
+  //     <GlobalStyle/>
+  //     <Button>Hello</Button>
+  //     <Button danger rotationTime={5}>Bye</Button>
+  //     <Anchor href="https://google.com">Go to google</Anchor>
+  //   </Container>
     
-  );
+  // );
 }
 
 const Container = styled.div`
@@ -26,37 +27,37 @@ const Container = styled.div`
   background-color: #fed330;
 `
 
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => props.danger ? "#fc5c65" : "#26de81" };
-  ${props => {
-    if (props.danger) {
-      return css`animation: ${rotation} ${props.rotationTime}s linear infinite;`
-    }
-  }}
-`
+// const Button = styled.button`
+//   border-radius: 50px;
+//   padding: 5px;
+//   min-width: 120px;
+//   color: white;
+//   font-weight: 600;
+//   appearance: none;
+//   cursor: pointer;
+//   &:active,
+//   &:focus {
+//     outline: none;
+//   }
+//   background-color: ${props => props.danger ? "#fc5c65" : "#26de81" };
+//   ${props => {
+//     if (props.danger) {
+//       return css`animation: ${rotation} ${props.rotationTime}s linear infinite;`
+//     }
+//   }}
+// `
 
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration:none;
-`
+// const Anchor = styled(Button.withComponent("a"))`
+//   text-decoration:none;
+// `
 
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
+// const rotation = keyframes`
+//   from {
+//     transform: rotate(0deg);
+//   }
+//   to {
+//     transform: rotate(360deg);
+//   }
+// `
 
 export default App;
