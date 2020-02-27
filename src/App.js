75775fdbe9e389a21dacbf2a1e6166457843ea82
jisoo <jsoo744@gmail.com>
2020-02-27 13:54:19 +0900

    [*] Commented Input

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const GlobalStyle = createGlobalStyle`
   } 
 `
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`
+// const awesomeCard = css`
+//   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+//   background-color: white;
+//   border-radius: 10px;
+//   padding: 20px;
+// `
 
 const Container = styled.div`
   height:100vh;
@@ -21,18 +21,21 @@ const Container = styled.div`
   background-color: #fed330;
 `
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  ${awesomeCard}
-`;
+// const Input = styled.input.attrs({
+//   required: true
+// })`
+//   border: none;
+//   ${awesomeCard}
+// `;
 
 function App() {
-  return (<Container>
-    <GlobalStyle/>
-    <Input placeholder="hello"></Input>
-  </Container>);
+  return (<Container></Container>);
+
+  // return (<Container>
+  //   <GlobalStyle/>
+  //   <Input placeholder="hello"></Input>
+  // </Container>);
+
   // return (
   //   <Container>
   //     <GlobalStyle/>
