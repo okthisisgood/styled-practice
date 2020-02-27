bbef58d449b940f33f534ade1d0ea7dbd36887e7
jisoo <jsoo744@gmail.com>
2020-02-27 13:49:34 +0900

    [*] Use mixins

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ const GlobalStyle = createGlobalStyle`
   } 
 `
 
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`
+
+const Container = styled.div`
+  height:100vh;
+  width:100%;
+  background-color: #fed330;
+`
+
 const Input = styled.input.attrs({
   required: true
 })`
-  border-radius: 5px;
+  border: none;
+  ${awesomeCard}
 `;
 
 function App() {
   return (<Container>
+    <GlobalStyle/>
     <Input placeholder="hello"></Input>
   </Container>);
   // return (
@@ -28,12 +43,6 @@ function App() {
     
   // );
 }
-
-const Container = styled.div`
-  height:100vh;
-  width:100%;
-  background-color: #fed330;
-`
 
 // const Button = styled.button`
 //   border-radius: 50px;
